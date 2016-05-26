7d4bbbb530e2c5e91f7efc9f5fb967c2fa59fb70
Michael Gaganov <gaganov@gmail.com>
2016-05-26 14:03:50 +0300

    removed mixin of findIndex, as it is implemented in lodash

--- vendor/assets/javascripts/s3_multipart/lib.js
+++ vendor/assets/javascripts/s3_multipart/lib.js
@@ -1,17 +1,6 @@
 (function(global) {
   global.S3MP = (function() {
 
-    // Wrap this into underscore library extension
-    _.mixin({
-      findIndex : function (collection, filter) {
-        for (var i = 0; i < collection.length; i++) {
-          if (filter(collection[i], i, collection)) {
-            return i;
-          }
-        }
-        return -1;
-      }
-    });
 // S3MP Constructor
 function S3MP(options) {
   var files
