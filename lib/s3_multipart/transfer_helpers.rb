18607b2593fade4486e2109bde31876f723db7a5
Dmitriy Kononov <kononov.dmitriy@gmail.com>
2018-09-26 13:39:52 +0600

    debug

--- lib/s3_multipart/transfer_helpers.rb
+++ lib/s3_multipart/transfer_helpers.rb
@@ -19,6 +19,10 @@ module S3Multipart
       p 555
       response = Http.post url, headers: headers
       parsed_response_body = XmlSimple.xml_in(response.body)
+      p url
+      p headers
+      p response
+      p response.body
       p 666
       { "key"  => parsed_response_body["Key"][0],
         "upload_id"   => parsed_response_body["UploadId"][0],
